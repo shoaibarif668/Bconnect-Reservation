8640871cd6b3283ff23c410dd466426aa3adac08
Kenneth Mckrola <kmckrola@dapdash.com>
2022-07-11 23:47:01 -0600

    switching to main

--- components/modals/EditCampaign.vue
+++ components/modals/EditCampaign.vue
@@ -2,9 +2,9 @@
     <div v-show="show_modal" @click="closeModal" class="fixed top-0 bottom-0 left-0 right-0 z-50 items-center justify-center p-6 overflow-auto bg-black bg-opacity-50">
         <div @click.stop class="p-6 bg-white rounded sm:mx-auto md:w-1/2 xl:w-1/4">
             Edit Text Message Campaign
-            <form action="">
+            <form @submit.prevent="">
                 <div>
-                    
+
                 </div>
             </form>
         </div>
@@ -23,6 +23,9 @@ export default {
         }
     },
     methods: {
+        submitChanges: function() {
+            
+        },
         closeModal() {
             this.$emit('close_modal');
         }
