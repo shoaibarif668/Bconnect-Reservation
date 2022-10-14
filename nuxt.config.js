3bbf6f4d5af601f6e1bbcf209cec263f45adc993
Shoaib Arif <shoaib.techxcape@gmail.com>
2022-10-14 05:50:34 +0500

    fix/ fixed path error

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
-
+  generate:{
+    fallback:true
+  },
   // publicRuntimeConfig: {
   //   baseURL: process.env.API_URL || 'http://localhost:8080',
   // },
